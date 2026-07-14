07d4c89531a2d6e0b22ff75f728d96ae7bddfacd
Gab <gabionian7@gmail.com>
2026-07-14 09:54:33 +0800

    List device license

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { errorHandler } from "@/middleware/errorHandler";
 
 export function createApp() {
   const app = express();
-
+  app.set("trust proxy", 1);
   app.use(helmet());
   app.use(
     cors({
